193b833f79213a50520a72383503bb248b726bab
michellepk <49343326+michellepk@users.noreply.github.com>
2026-09-16 17:38:21 +0000

    Solve: Scalar Multiplication of a Matrix

---
+++ problems/0005-scalar-multiplication-of-a-matrix/solution.py
@@ -0,0 +1,6 @@
+import numpy as np
+def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
+	# Your code here
+	a = np.array(matrix)
+	a = scalar*a
+	return a
